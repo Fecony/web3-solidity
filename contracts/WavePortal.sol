e5d7215894666e6eba84b8d02cb60f8d52c22b81
Ricards Tagils <tagils@mail.ru>
2022-07-26 10:05:24 +0400

    feat: add deploy and method to retrieve wave count for address

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,27 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint totalWaves;
+
+    mapping(address => uint) addressWave;
+
     constructor() {
         console.log("Yo, Farza. Don't be sad!");
+    }
+
+    function wave() public {
+        totalWaves++;
+        addressWave[msg.sender]++;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getTotalWavesFor(address _address) public view returns (uint256) {
+        console.log("%s has waved %d times!", _address, addressWave[_address]);
+        return addressWave[_address];
     }
 }
